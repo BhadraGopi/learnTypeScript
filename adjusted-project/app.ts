5ff587a97925607e626b7e4abfbf324572829d2d
BhadraGopi <bhadramadathikunnel@gmail.com>
2022-01-29 18:04:40 +0530

    Array Objects

--- adjusted-project/app.ts
+++ adjusted-project/app.ts
@@ -2,8 +2,9 @@ const person:{
     name:string;
     age:number;
 }={
+   
     name: 'Doll',
-    age:80
+    age:80, 
 };
 console.log(person.name);
 //this does not create an object of type name and age, it just helps ts understand how the obj person would look like
@@ -28,3 +29,15 @@ const product = {
 //       title: string;
 //       description: string;
 //     }
+
+//ArrayObjects
+const person2={
+    name: 'Doll',
+    age:80, 
+    hobbies:['read','sports']
+};
+for(const  hobby of person2.hobbies){
+    console.log(hobby.toLocaleUpperCase()); //this possible bcoz ts idetifies that hobbies is an array of string
+//console.log(hobby.map()); //ERROR as hobby will have string and .map operates on arrays.
+}
+let names: string[];//implies names is an array of the type string meaning it will only contain string values
