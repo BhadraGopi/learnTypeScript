cbe0dbb59aa24dff84207feb66a47327ac2aba82
BhadraGopi <bhadramadathikunnel@gmail.com>
2022-01-29 17:46:46 +0530

    Example of an object Type

--- adjusted-project/app.js
+++ adjusted-project/app.js
@@ -1,25 +1,21 @@
-const person:{
-    name:string;
-    age:number;
-}={
+var person = {
     name: 'Doll',
-    age:80
+    age: 80
 };
 console.log(person.name);
 //this does not create an object of type name and age, it just helps ts understand how the obj person would look like
 //would work perf without it too
 //Object types can be created for nested objects as well.
-const product = {
+var product = {
     id: 'abc1',
     price: 12.99,
     tags: ['great-offer', 'hot-and-new'],
     details: {
-      title: 'Red Carpet',
-      description: 'A great carpet - almost brand-new!'
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
     }
-  }
-  //object type of the same would be the following
-  
+};
+//object type of the same would be the following
 //   {
 //     id: string;
 //     price: number;
