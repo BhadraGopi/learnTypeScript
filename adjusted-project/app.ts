fc8ccba9b5e132ba8c5839bfeeb53e2bdf18a08c
BhadraGopi <bhadramadathikunnel@gmail.com>
2022-01-31 10:24:47 +0530

    Working with Tuple

--- adjusted-project/app.ts
+++ adjusted-project/app.ts
@@ -1,11 +1,16 @@
 const person:{
     name:string;
     age:number;
+    role:[number,string];//a Tuple declaration implies first elm of role must be a number and 2nd a string and role will strictly be of length 2.
 }={
    
     name: 'Doll',
     age:80, 
+    role:[3,'author']
 };
+person.role.push('admin');//doesn't show error as TS can't over write .push method
+//person.role[1]=9; throws error
+//person.role=[0,'admin,'user']; also throws ERROR
 console.log(person.name);
 //this does not create an object of type name and age, it just helps ts understand how the obj person would look like
 //would work perf without it too
@@ -34,10 +39,11 @@ const product = {
 const person2={
     name: 'Doll',
     age:80, 
-    hobbies:['read','sports']
+    hobbies:['read','sports'],
+    
 };
 for(const  hobby of person2.hobbies){
     console.log(hobby.toLocaleUpperCase()); //this possible bcoz ts idetifies that hobbies is an array of string
 //console.log(hobby.map()); //ERROR as hobby will have string and .map operates on arrays.
 }
-let names: string[];//implies names is an array of the type string meaning it will only contain string values
+let names: string[];//implies names 
