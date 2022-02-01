1af23207306b0df74479bd2acc4a078aee129b7a
BhadraGopi <bhadramadathikunnel@gmail.com>
2022-02-01 13:53:43 +0530

    Creating a class

--- adjusted-project/src/app.ts
+++ adjusted-project/src/app.ts
@@ -1,1 +1,10 @@
-console.log('THis works!');
+class Department{
+    name: string; //field of the class NOT an object
+  
+    constructor(n:string){
+this.name=n;
+    }
+}
+const accounting= new Department('Accounting');//the object is initialized
+console.log(accounting);
+//op will be Department {name:'Accounting'}
