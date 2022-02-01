f9439e546e0d1150d44b75ce6f623f9deefcd8d5
BhadraGopi <bhadramadathikunnel@gmail.com>
2022-02-01 09:38:47 +0530

    Arrow Functions

--- adjusted-project/app.ts
+++ adjusted-project/app.ts
@@ -7,7 +7,7 @@
   //any type of values can be assigned but its diff from any type
  // userName=userinput; //ERROR 
  userName=anyUserInput;//No error viz the main diff
- //Inorder to avoid the error while using unknown just check the type bfore using
+ //Inorder to avoid the error while using unknown just check the type bfore u sing
  if(typeof userinput === 'string'){
      userName= userinput;
      //Here userinput is guarenteed to be a string here
@@ -23,3 +23,13 @@
  //this actually doesn't show anything not even undefined 
  //thats coz the fn returns a never value by default althoughthe hover shows void
  //It's actually never as this  function breaks the script another example would be a function with an infinite loop.
+
+
+// ARROW FUNCTIONS
+
+// const  PrintOUT: void(x:number|string) {
+//     console.log(x);
+// } this can actually be written as below(as it contains only one arguemt )
+const printOUT:(a:number|string)=>void = output=>console.log(output); 
+//If there are no parameters an empty pair of parantheses is used 
+()=>{}//Like so
