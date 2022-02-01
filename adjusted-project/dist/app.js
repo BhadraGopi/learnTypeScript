6c49d57253efb4d5d274a8fb171c715a442d7964
BhadraGopi <bhadramadathikunnel@gmail.com>
2022-02-01 22:57:39 +0530

    Type Guards

--- adjusted-project/dist/app.js
+++ adjusted-project/dist/app.js
@@ -1,7 +1,46 @@
 "use strict";
 const e1 = {
     name: 'Kim',
-    priviledgess: ['create-server'],
+    priviledges: ['create-server'],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        //this is a type guard using typeof
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInfo(emp) {
+    console.log('Name:' + emp.name);
+    if ('priviledges' in emp) {
+        console.log('Priviledges:' + emp.priviledges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date:' + emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+class Car {
+    drive() {
+        console.log('Driving a Car..');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a Truck..');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(900);
+        //JS idendentifies instanceof
+    }
+}
 //# sourceMappingURL=app.js.map
--- adjusted-project/dist/app.ts
+++ adjusted-project/dist/app.ts
@@ -1,7 +1,7 @@
 //Intersection Types
 type Admin={
     name: string;
-    priviledgess:string[];
+    priviledges:string[];
 };
 type Employee={
     name: string;
@@ -11,9 +11,48 @@ type Employee={
 type ElevatedEmployee=Admin&Employee;
 const e1: ElevatedEmployee={
     name:'Kim',
-    priviledgess:['create-server'],
+    priviledges:['create-server'],
     startDate: new Date()
 }
 type Combinable = string|number;
 type Numeric=number|boolean;
 type Universal= Combinable& Numeric;
+function add(a:Combinable,b:Combinable){
+    if(typeof a==='string' || typeof b==='string'){
+        //this is a type guard using typeof
+        return a.toString()+ b.toString();
+    }
+    return a+b;
+}
+type UnknownEmployee= Employee|Admin;
+function printEmployeeInfo(emp: UnknownEmployee){
+    console.log('Name:'+emp.name);
+    if('priviledges' in emp){
+        console.log('Priviledges:'+ emp.priviledges);
+    }
+    if('startDate' in emp){
+        console.log('Start Date:'+ emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+class Car{
+    drive(){
+        console.log('Driving a Car..');
+}}
+class Truck{
+    drive(){
+        console.log('Driving a Truck..');
+}
+loadCargo(amount:number){
+    console.log('Loading cargo...'+ amount);
+}
+}
+type Vehicle= Car|Truck;
+const v1=new Car(); const v2= new Truck();
+function useVehicle(vehicle:Vehicle){
+    vehicle.drive();
+    if(vehicle instanceof Truck){
+        vehicle.loadCargo(900);
+        //JS idendentifies instanceof
+    }
+}
